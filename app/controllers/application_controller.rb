601a091c3d12e23290e56c0dc1404afc1348a885
Jaasim <jaasimpolin@gmail.com>
2015-07-12 18:26:33 -0400

    fixed attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ end
   protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,:student,:admin,:uni_admin,:event_creator) }
-        devise_parameter_sanitizer.for(:event.new) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth, :image) }
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,:student,:admin,:uni_admin,:event_creator,:password_confirmation) }
+        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :dob, :image) }
     end
 
 
