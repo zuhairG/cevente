e1dc327a2311732ab7110ad72b17458737e383f7
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2015-07-12 17:50:28 -0400

    ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+rescue_from CanCan::AccessDenied do |e|
+    flash[:error] = "Access Denied"
+    redirect_to root_url
+
+end
+
   protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth, :avatar) }
+        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,:student,:admin,:uni_admin,:event_creator) }
+        devise_parameter_sanitizer.for(:event.new) { |u| u.permit(:name, :email, :password, :current_password, :is_female, :date_of_birth, :image) }
     end
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+  #attr_accessible :admin, :student, :uni_admin, :event_creator
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   # Include default devise modules. Others available are:
@@ -9,5 +11,6 @@ class User < ActiveRecord::Base
    has_many :events , :through => :user_events
    has_many  :organization , :through => :user_org
    has_many :rsvps
+
 
 end
