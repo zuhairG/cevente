572913bd7784b20b59306756e1c9a50425982c48
acheweiky <acheweiky@gmail.com>
2015-10-24 21:15:06 -0500

    Footer added and seperated by partials

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,13 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( footer.css )
+Rails.application.config.assets.precompile += %w( application.css )
+Rails.application.config.assets.precompile += %w( font-awesome.css )
+
+Rails.application.config.assets.precompile += %w( bootstrap_and_override.css.less )
+
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
