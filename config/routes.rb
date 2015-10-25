b3d6e5f3c16a07279eef41ef6e469fa57c3ad1d0
Zuhair Ghalib <zuhair.ghalib@gmail.com>
2015-10-25 16:31:36 -0400

    signup fields added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+  get "users/sign_in", :to => "devise/sessions#new"
+  get "users/sign_out", :to => "devise/sessions#destroy"
+  get "users/sign_up", :to => "devise/registrations#new"
+end
+
   resources :user_orgs
   resources :event_types
   resources :rsvps
